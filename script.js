1cde1757461348b008bc1e58585e66edad7ea4d3
jake rudlong <jrudlong@gmail.com>
2023-02-16 14:48:35 -0600

    adds function for efficiently retrieving data from local storage and storing it in the appropriate text area

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-//define vars 
-var timeRow = $(".time-block");
-var saver = $(".saveBtn");
-var messageBox = [];
-var importantText;
-var babySibling;
-var mamaWho;
-
+//define vars
+var timeRow = $(".time-block"); // quick reference to our time blocks
+var saver = $(".saveBtn"); // quick ref for buttons with class "saveBtn"
 // wait to page loads to run stuff
 $(document).ready(function() {
   let today = dayjs();
@@ -28,30 +23,22 @@ $(document).ready(function() {
     }
   });
 
-  // useful when saving the description in local storage?
-  
-    $(timeRow).on('click', saver, function() {
-      let currentThing = $(this).attr("id");
-      textToSave = $(this).children(".description").val();
-      localStorage.setItem(currentThing, textToSave);
+    $(timeRow).on('click', saver, function() { //all time blocks wait for a click event from a saver button
+      let currentThing = $(this).attr("id"); // get the id from the time block
+      textToSave = $(this).children(".description").val(); //whatever the value of the text area with class of "description" within that timeblock, is what we want to save.
+      localStorage.setItem(currentThing, textToSave); //save that text to local storage using the specific time block's id as the key (which will be needed for retrieval)
     });
   
-  function getData() {
-
-  }
+  function getMyData() { //only loop through what's actually in storage
+    for (i = 0; i < localStorage.length; i++) {
+      let x = localStorage.key(i); //x takes value of storage key (which was set as the id of whatever time block had text saved);
+      let rightPlace = $("#"+x); //gets us to the right time box when we search by id (which x captures); we could also use vanilla/standard js and do getElementById(x) to get the same result.
+      messageForBaby = localStorage.getItem(x); //get our text/value out of storage for the current key (x)
+      $(rightPlace).children(".description").text(messageForBaby); //set that message as the text for our textarea element (look for child of current time block with class == "description").
+    };
+  };
+  getMyData(); //we want this called whenever the page is refreshed/loaded
     
-    
-
-}); //this closes everything
-
+}); //this closes everything inside the document.load thingy
 
   
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-  
-    // function getData() {
-    //   message = localStorage.getItem(mamaWho,msg);
-    //   $(babySibling).text(message);
-    //   console.log(message);
-    // }
