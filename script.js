7c01857a72da461ac221eddbc8bb982867ea06c1
jake rudlong <jrudlong@gmail.com>
2023-02-16 00:20:27 -0600

    addes elegent and efficient function to store input into local storage, adds readme

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 //define vars 
 var timeRow = $(".time-block");
 var saver = $(".saveBtn");
+var messageBox = [];
+var importantText;
+var babySibling;
+var mamaWho;
 
 // wait to page loads to run stuff
 $(document).ready(function() {
   let today = dayjs();
-  $('#currentDay').text(today.format('[Today is] MMM D YYYY')); // sets #currentDay id element's text to display the current day from dayjs in dd month(3ltr name) YYYY format.
-  let hour = today.hour(); // i want to have the up-to-date hour as soon as i load the page.
+  $('#currentDay').text(today.format('[Today is] MMM D YYYY')); // sets #currentDay id element's text to display the current day from dayjs
+  let hour = today.hour(); // get current hr 
 
-  
   // for each element with class .time-block, i will get the id, take the last two values of the string (the nubmers) and assign those as the value for currentThing (current appointment box thing)
   $(timeRow).each(function() {
     // if (this.attr("id").substring(5,6) < hour) {
@@ -22,52 +25,33 @@ $(document).ready(function() {
       $(currentThing).addClass("present");
     } else {
       $(currentThing).addClass("past");
-    };
-
-    function saveMe(mama) {
-      mama = mama;
-      console.log(mama);
-      // let thingToSave = $(#mama);
-      // console.log(thingToSave);
     }
-
-    $(timeRow).on("click", saver, function(event){
-      // console.log(event.target);
-      // event.stopPropagation();
-      let baby = event.target;
-      let mama = $(baby).parent();
-      let mamaWho = $(mama).attr("id");
-      // let msg = $(mamaWho).text();
-      console.log(mamaWho);
-      saveMe(mamaWho);
-    });
-    
-
   });
-});
-  
-  
-  // let value = 
-  
 
-  // $(function () {
+  // useful when saving the description in local storage?
+  
+    $(timeRow).on('click', saver, function() {
+      let currentThing = $(this).attr("id");
+      textToSave = $(this).children(".description").val();
+      localStorage.setItem(currentThing, textToSave);
+    });
+  
+  function getData() {
+
+  }
     
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
+    
+
+}); //this closes everything
+
+
+  
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
-    
-
-
-// TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
+  
+    // function getData() {
+    //   message = localStorage.getItem(mamaWho,msg);
+    //   $(babySibling).text(message);
+    //   console.log(message);
+    // }
